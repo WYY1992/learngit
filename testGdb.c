626d8c7ed308e0310a96ba12edfdee41d745d241
WYY <1024673005@qq.com>
2016-06-12 00:32:09 +0800

    Created for Gdb test

---
+++ testGdb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void sum(int m);
+
+void main ()
+{
+	int n = 0;
+	sum(50);
+	for (int i = 0; i<=50; i++)
+		n += i;
+	printf("The sum of 1-50 is %d \n", n);
+}
+
+void sum(int m)
+{
+	int i,n = 0;
+	for (i = 0; i <= m; i++)
+		n += i;
+	printf("The sum of 1~%d is %d", m, n);
+}
